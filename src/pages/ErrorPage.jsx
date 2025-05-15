ba9038179cd0654d643d5e06ce55600e4c37d6a0
Varda Joshi <varda.21j@gmail.com>
2025-05-15 23:22:31 +0530

    Added Error Page and assets folder

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import error from "../assets/error.png";
+
+export function ErrorPage() {
+  return (
+    <main>
+      <section className="flex flex-col pb-44 text-base text-center bg-leafGreen max-md:pb-24 max-md:max-w-full">
+        <img
+          src={error}
+          className="object-contain self-center mt-9 mb-0 max-w-full aspect-[1.5] w-[688px] max-md:mb-2.5"
+        />
+      </section>
+    </main>
+  );
+};

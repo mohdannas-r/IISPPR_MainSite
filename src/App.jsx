7d34c88ce20ec2438b52b1856ed746761a03cb9d
prakritik24052 <prakritik941@gmail.com>
2025-05-17 19:51:12 +0530

    contact form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ContactForm from './components/ContactForm';
 import Routes from './routes';
+
+
 import './App.css'
 
 const App = () => {
   return (
     <Router>
+      <ContactForm />
       <Routes />
     </Router>
   );

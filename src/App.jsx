a61e415d0214c02076a07e8974fd2639e11ec229
HerambInamke <heramb15012006@gmail.com>
2025-05-19 20:05:15 +0530

    done everything just needed the real data

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
+import Error from './pages/Error';
+import NotFound from './pages/NotFound';
+import SearchNotFound from './pages/SearchNotFound';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <main className="pt-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/error" element={<Error />} />
+            <Route path="/search-not-found" element={<SearchNotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
